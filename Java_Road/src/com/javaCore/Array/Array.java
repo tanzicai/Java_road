8bec64368cccb2b503ce5b968158807cdc14706c
tanzicai <1728561744@qq.com>
2020-12-27 03:24:54 +0800

    声明数组、数组输入、数组遍历等

--- Java_Road/src/com/javaCore/Array/Array.java
+++ Java_Road/src/com/javaCore/Array/Array.java
@@ -1,4 +1,35 @@
 package com.javaCore.Array;
 
 public class Array {
+    public static void main(String[] args){
+        //数组创建
+        //1.静态创建方法：内容开始知道
+        //C-style array declaration of local variable 'age'
+        int age[] = {10,20,30};
+        //Java-style declaration
+        String[] names = {"zhangsan","lisi"};
+
+        //动态创建:内容开始不知道
+        //C-style array declaration of local variable 'age'
+        int age2[] = new int[10 ];
+        //Java-style declaration
+        String[] name2  =new String[10];
+
+        //数据输入
+        for (int i=0 ; i < 10;i++){
+            age2[i] = i+1;
+        }
+        for (int num : age2)System.out.println(num);
+        //数组声明示例
+        /*
+        int[] a;      = int[] a;
+        int b[];      = int[] b;
+        int[] c [];   = int[][] c;
+        int[] d,e;    = int[] d;   int[] e;
+        int f,g[];    = int f;   int[] g;
+        int[] h,i,j[];= int[]h;   int i[];   int[][] j;
+        */
+
+
+    }
 }
